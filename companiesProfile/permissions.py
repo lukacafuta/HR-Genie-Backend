5d7047708a4b4f728c4dbe3a1c6f1a6956cbc82f
lukacafuta <luka.cafuta@gmail.com>
2024-07-09 17:39:14 +0200

    updated

--- companiesProfile/permissions.py
+++ companiesProfile/permissions.py
@@ -7,9 +7,16 @@ from userProfile.models import UserProfile
 class IsCompanyAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         company_id = view.kwargs.get('pk')
-        company = get_object_or_404(CompaniesProfile, pk=company_id)
-        try:
-            user_profile = UserProfile.objects.get(customUser=request.user, company=company)
-            return user_profile.isCompanyAdmin
-        except UserProfile.DoesNotExist:
+
+        if company_id:
+            # retrieve the company profile using the company ID
+            company = get_object_or_404(CompaniesProfile, pk=company_id)
+
+            try:
+                # check if the user is an admin of the given company
+                user_profile = UserProfile.objects.get(customUser=request.user, company=company)
+                return user_profile.isCompanyAdmin
+            except UserProfile.DoesNotExist:
+                return False
+        else:
             return False
