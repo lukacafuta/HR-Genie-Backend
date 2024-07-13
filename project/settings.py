4e521a36e383d40791c352fdaedbafc716531b4e
lukacafuta <luka.cafuta@gmail.com>
2024-07-13 19:30:09 +0200

    corrected ast.eval

--- project/settings.py
+++ project/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DJANGO_DEBUG = ast.literal.eval(os.environ.get("DJANGO_DEBUG", True))
+DJANGO_DEBUG = ast.literal_eval(os.environ.get("DJANGO_DEBUG", True))
 
 ALLOWED_HOSTS = ['hr-genie-backend-24b07ef76680.herokuapp.com', 'localhost']
 
