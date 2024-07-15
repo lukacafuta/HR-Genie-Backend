a1f117c02b3fd3de4fb2738db22c5a8ea6489d47
lukacafuta <luka.cafuta@gmail.com>
2024-07-15 20:07:07 +0200

    added avatar field in models and admin, updated settings for 'media' and 'static' files, updated .gitignore (media, static)

--- project/urls.py
+++ project/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt import views as jwt_views     #JWT
@@ -22,6 +23,9 @@ from rest_framework_simplejwt import views as jwt_views     #JWT
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+
+import settings
+
 # ... SWAGGER
 
 schema_view = get_schema_view(
@@ -74,3 +78,7 @@ urlpatterns = [
     path('backend/api/docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     # ... SWAGGER
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
