8e864fba2a4d88b4c5e73d0cf5ce19055d3daf85
lukacafuta <luka.cafuta@gmail.com>
2024-07-16 19:00:24 +0200

    updated settings in urls.py

--- project/urls.py
+++ project/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
@@ -24,7 +25,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-import settings
+
 
 # ... SWAGGER
 
