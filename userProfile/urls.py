ee694e75bee3b127d01f270848c74cdbf7dab685
Daniele Scopece <daniele.scopece@gmail.com>
2024-07-23 21:24:15 +0200

    daniele_240723.eve.02 endpoint user/mycompany/

--- userProfile/urls.py
+++ userProfile/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 #from .views import UserProfileGetAll, UserProfileByApproverView
-from .views import UserProfileGetAll, ListUserProfileMeView, UserProfileByApproverView
+from .views import UserProfileGetAll, ListUserProfileMeView, UserProfileByApproverView, ListUserProfileMyCompanyView
 
 urlpatterns = [
     path('', UserProfileGetAll.as_view()),
@@ -9,4 +9,5 @@ urlpatterns = [
     #path('<int:pk>/', UserProfileGetAll.as_view()),
     # path('<int:pk>/', RecipeByIdView.as_view())
     path('me/', ListUserProfileMeView.as_view()),
+    path('mycompany/', ListUserProfileMyCompanyView.as_view()),
 ]
